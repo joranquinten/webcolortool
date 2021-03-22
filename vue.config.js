1f72d427135a00772bb4eee19162add01ae0e371
Joran Quinten <joran.quinten@jumbo.com>
2021-03-22 14:09:44 +0100

    dev(): add deploy script

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,19 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
+        appId: "com.electron.webcolortool",
+        productName: "WebColorTool",
         win: {
+          target: [
+            {
+              target: "msi",
+              arch: ["x64", "ia32"]
+            },
+            {
+              target: "portable",
+              arch: ["x64", "ia32"]
+            }
+          ],
           icon: "./public/favicon.ico"
         }
       }
